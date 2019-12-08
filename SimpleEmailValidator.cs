172d720d00dbf4b47a27618c9d4ac72d05c7b38a
Simplevolk <simplevolk@gmail.com>
2019-12-08 17:40:52 +0300

    init2

---
+++ SimpleEmailValidator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace SimpleEmailValidator
+{
+    public class SimpleEmailValidator
+    {
+        bool _invalid = false;
+        /// <summary>
+        /// is email valid? (checked by regexp)
+        /// </summary>
+        /// <param name="email">input email</param>
+        /// <returns>true- email is valid, false- not valid</returns>
+        public bool IsValidEmailByRegexp(string email)
+        {
+            
+            if (String.IsNullOrEmpty(email))
+                return false;
+
+            // Use IdnMapping class to convert Unicode domain names.
+            try
+            {
+                email = Regex.Replace(email, @"(@)(.+)$", this.DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+
+            if (_invalid)
+                return false;
+
+            // Return true if strIn is in valid e-mail format.
+            try
+            {
+                return Regex.IsMatch(email,
+                      @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                      @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                      RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// is email valid?
+        /// </summary>
+        /// <param name="email">input email string</param>
+        /// <returns>true- email is valid, false- not valid</returns>
+        bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private string DomainMapper(Match match)
+        {
+            // IdnMapping class with default property values.
+            IdnMapping idn = new IdnMapping();
+
+            string domainName = match.Groups[2].Value;
+            try
+            {
+                domainName = idn.GetAscii(domainName);
+            }
+            catch (ArgumentException)
+            {
+                _invalid = true;
+            }
+            return match.Groups[1].Value + domainName;
+        }
+    }
+}
